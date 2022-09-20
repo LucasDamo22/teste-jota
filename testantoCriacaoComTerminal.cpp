dc7e63e1b9f9ee1b0da60e8c825067556aa757af
Lucas Damo <lucas-damo@lucas.damo-VirtualBox>
2022-09-19 22:26:19 -0300

    teste de muitas coisas

---
+++ testantoCriacaoComTerminal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main (){
+    std::cout << "Testando o terminal"<< std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "Testando o terminal/fim"<< std::endl;
+}
